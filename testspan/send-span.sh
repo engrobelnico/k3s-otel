35c7c02aac32484c43aa20b0a11e2d99f559991e
NICOLAS LE BORGNE <nicolas.leborgne@louisvuitton.com>
2025-06-04 17:26:50 +0200

    add b3 span id header

--- testspan/send-span.sh
+++ testspan/send-span.sh
@@ -37,7 +37,7 @@ SendSpan(){
     sed -i "s|TRACE_ID|$traceID|g" ./test-client.json
     sed -i "s|DURATION_SPAN|$clientDuration|g" ./test-client.json
     sed -i "s|TIMESTAMP_SPAN|$timestampClient|g" ./test-client.json
-    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-client.json" -H  "accept: application/json" -H  "Content-Type: application/json"
+    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-client.json" -H  "accept: application/json" -H  "Content-Type: application/json" -H "X-B3-TraceId: $traceID" 
     sed -i "s|$traceID|TRACE_ID|g" ./test-client.json
     sed -i "s|$timestampClient|TIMESTAMP_SPAN|g" ./test-client.json
     sed -i "s|$clientDuration|DURATION_SPAN|g" ./test-client.json
@@ -47,7 +47,7 @@ SendSpan(){
     sed -i "s|TRACE_ID|$traceID|g" ./test-server.json
     sed -i "s|DURATION_SPAN|$server1Duration|g" ./test-server.json
     sed -i "s|TIMESTAMP_SPAN|$timestampServer1|g" ./test-server.json
-    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-server.json" -H  "accept: application/json" -H  "Content-Type: application/json"
+    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-server.json" -H  "accept: application/json" -H  "Content-Type: application/json" -H "X-B3-TraceId: $traceID" 
     sed -i "s|$traceID|TRACE_ID|g" ./test-server.json
     sed -i "s|$timestampServer1|TIMESTAMP_SPAN|g" ./test-server.json
     sed -i "s|$server1Duration|DURATION_SPAN|g" ./test-server.json
@@ -57,7 +57,7 @@ SendSpan(){
     sed -i "s|TRACE_ID|$traceID|g" ./test-server2.json
     sed -i "s|DURATION_SPAN|$server2Duration|g" ./test-server2.json
     sed -i "s|TIMESTAMP_SPAN|$timestampServer2|g" ./test-server2.json
-    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-server2.json" -H  "accept: application/json" -H  "Content-Type: application/json"
+    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-server2.json" -H  "accept: application/json" -H  "Content-Type: application/json" -H "X-B3-TraceId: $traceID" 
     sed -i "s|$traceID|TRACE_ID|g" ./test-server2.json
     sed -i "s|$timestampServer2|TIMESTAMP_SPAN|g" ./test-server2.json
     sed -i "s|$server2Duration|DURATION_SPAN|g" ./test-server2.json
@@ -72,7 +72,7 @@ SendSpan(){
     sed -i "s|TIMESTAMP_SPAN|$timestampRemoteServer|g" ./test-remoteserver.json
     sed -i "s|START_REMOTE|$timestampRemoteServerStartRemote|g" ./test-remoteserver.json
     sed -i "s|STOP_REMOTE|$timestampRemoteServerStopRemote|g" ./test-remoteserver.json
-    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-remoteserver.json" -H  "accept: application/json" -H  "Content-Type: application/json"
+    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-remoteserver.json" -H  "accept: application/json" -H  "Content-Type: application/json" -H "X-B3-TraceId: $traceID" 
     sed -i "s|$traceID|TRACE_ID|g" ./test-remoteserver.json
     sed -i "s|$timestampRemoteServer|TIMESTAMP_SPAN|g" ./test-remoteserver.json
     sed -i "s|$timestampRemoteServerStartRemote|START_REMOTE|g" ./test-remoteserver.json
@@ -84,7 +84,7 @@ SendSpan(){
     sed -i "s|TRACE_ID|$traceID|g" ./test-server3.json
     sed -i "s|DURATION_SPAN|$remoteDuration|g" ./test-server3.json
     sed -i "s|TIMESTAMP_SPAN|$timestampServer3|g" ./test-server3.json
-    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-server3.json" -H  "accept: application/json" -H  "Content-Type: application/json"
+    curl --insecure --write-out "%{http_code}\n" -X POST $serverURI --data "@./test-server3.json" -H  "accept: application/json" -H  "Content-Type: application/json" -H "X-B3-TraceId: $traceID" 
     sed -i "s|$traceID|TRACE_ID|g" ./test-server3.json
     sed -i "s|$timestampServer3|TIMESTAMP_SPAN|g" ./test-server3.json
     sed -i "s|$remoteDuration|DURATION_SPAN|g" ./test-server3.json
@@ -95,7 +95,7 @@ SendSpan(){
     sed -i "s|TRACE_ID|$traceID|g" ./test-otel.json
     sed -i "s|DURATION_SPAN|$durationotel|g" ./test-otel.json
     sed -i "s|TIMESTAMP_SPAN|$timestampotel|g" ./test-otel.json
-    curl --insecure --write-out "%{http_code}\n" -X POST $serverOTEL --data "@./test-otel.json" -H  "accept: application/json" -H  "Content-Type: application/json"
+    curl --insecure --write-out "%{http_code}\n" -X POST $serverOTEL --data "@./test-otel.json" -H  "accept: application/json" -H  "Content-Type: application/json" -H "X-B3-TraceId: $traceID" 
     sed -i "s|$traceID|TRACE_ID|g" ./test-otel.json
     sed -i "s|$timestampotel|TIMESTAMP_SPAN|g" ./test-otel.json
     sed -i "s|$durationotel|DURATION_SPAN|g" ./test-otel.json
